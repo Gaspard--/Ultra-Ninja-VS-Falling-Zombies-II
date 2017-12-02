41a1dbf6811ac2fb598cc31ad6fea58d2fc87163
Riamon Vincent <vincent.riamon@epitech.eu>
2017-12-02 23:59:57 +0100

    mutation..

--- source/MobManager.cpp
+++ source/MobManager.cpp
@@ -1,1 +1,16 @@
 #include "MobManager.hpp"
+
+void MobManager::mutation()
+{
+  std::vector<Human>::iterator bound;
+
+  bound = std::partition(humans.begin(), humans.end(), [](auto const &human)
+			 {
+			   return !human.getInfected();
+			 });
+  for (auto it(bound); it != humans.end(); ++it)
+    {
+      zombies.push_back(*it);
+    }
+  humans.erase(bound, humans.end());
+}
