c80d7daf8ddca314fd21843afe81a0bb242055dc
melis_m <matteo.melis@epitech.eu>
2017-12-03 22:18:34 +0100

    added circle attack to Player

--- include/Player.hpp
+++ include/Player.hpp
@@ -4,6 +4,8 @@
 #include "Entity.hpp"
 #include "Zombie.hpp"
 
+class Slash;
+
 class Player
 {
 public:
@@ -15,6 +17,7 @@ public:
   void update();
   // dir[0] = x axis , dir[1] = y axis
   void accelerate(Vect<2, int> const& dir);
+  void circleAttack(std::vector<Slash> &);
 };
 
 #endif //!PLAYER_HPP_
--- source/Player.cpp
+++ source/Player.cpp
@@ -1,3 +1,4 @@
+#include "Slash.hpp"
 #include "Player.hpp"
 
 Player::Player(Entity entity)
@@ -17,4 +18,20 @@ void Player::accelerate(Vect<2, int> const& dir)
 {
   Vect<2, double> vec(0.005, 0.005);
   this->entity.fixture.speed += vec * dir;
+}
+
+void Player::circleAttack(std::vector<Slash> &slashes)
+{
+  static constexpr double posOffset = 0.1;
+  static constexpr double speed = 0.01;
+  auto &pos = entity.fixture.pos;
+
+  slashes.emplace_back(Vect<2, double>(pos[0], pos[1] - posOffset), Vect<2, double>(0.0, -speed), 2); // UP
+  slashes.emplace_back(Vect<2, double>(pos[0] + posOffset, pos[1] - posOffset), Vect<2, double>(speed, -speed), 2); // UP RIGHT
+  slashes.emplace_back(Vect<2, double>(pos[0] + posOffset, pos[1]), Vect<2, double>(speed, 0.0), 2); // RIGHT
+  slashes.emplace_back(Vect<2, double>(pos[0] + posOffset, pos[1] + posOffset), Vect<2, double>(speed, speed), 2); // DOWN RIGHT
+  slashes.emplace_back(Vect<2, double>(pos[0], pos[1] + posOffset), Vect<2, double>(0.0, speed), 2); // DOWN
+  slashes.emplace_back(Vect<2, double>(pos[0] - posOffset, pos[1] + posOffset), Vect<2, double>(-speed, speed), 2); // DOWN LEFT
+  slashes.emplace_back(Vect<2, double>(pos[0] - posOffset, pos[1]), Vect<2, double>(-speed, 0.0), 2); // LEFT
+  slashes.emplace_back(Vect<2, double>(pos[0] - posOffset, pos[1] - posOffset), Vect<2, double>(-speed, -speed), 2); // UP LEFT
 }
