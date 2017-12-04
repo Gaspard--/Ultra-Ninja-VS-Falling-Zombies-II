edce2d05c6071e77832de665af851d6ed04d5528
David Zeng <david.zeng@epitech.eu>
2017-12-04 15:54:24 +0100

    Modified nb of zombie

--- source/Logic.cpp
+++ source/Logic.cpp
@@ -18,8 +18,8 @@ Logic::Logic()
   gameOver = false;
   combo = 0;
   multiplier = 0;
-  for (int i = 0 ; i < 20 ; ++i)
-    for (int j = 0 ; j < 20 ; ++j)
+  for (int i = 0 ; i < 2 ; ++i)
+    for (int j = 0 ; j < 2 ; ++j)
       entityManager.spawnZombie({i * 1.5 + 40.0, j * 1.5 + 40.0});
   entityManager.spawnHuman({50.0, 50.0}, block);
   entityManager.spawnPlayer({50.4, 50.3});
