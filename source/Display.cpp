51a5bbf5b062804e6191031b0011945068e515b1
Gaspard-- <jakob.kellendonk@epitech.eu>
2017-12-04 22:40:19 +0100

    fixed camera

--- source/Display.cpp
+++ source/Display.cpp
@@ -305,9 +305,9 @@ void Display::copyRenderData(Logic const &logic)
   auto const gamePos(logic.getPlayerPos());
   camera.offset = (camera.offset + offset) * 0.5f - gamePos * 0.5f - offset;
 
-  camera.offset[0] = std::max(std::min(camera.offset[0], -100.0 + 1.0 / (dim[0] * camera.zoom[0])),
+  camera.offset[0] = std::min(std::max(camera.offset[0], -100.0 + 1.0 / (dim[0] * camera.zoom[0])),
 			      -1.0 / (dim[0] * camera.zoom[0]));
-  camera.offset[1] = std::max(std::min(camera.offset[1], -100.0 + 1.0 / (dim[1] * camera.zoom[1])),
+  camera.offset[1] = std::min(std::max(camera.offset[1], -100.0 + 1.0 / (dim[1] * camera.zoom[1])),
 			      -1.0 / (dim[1] * camera.zoom[1]));
   displayInfo.time = logic.getTime();
   displayInfo.score = logic.getScore();
